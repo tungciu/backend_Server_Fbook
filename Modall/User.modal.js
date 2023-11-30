0c87b918002b1f99bdf083e147fe95ee8c4345a4
tungmvph <mtung743@gmail.com>
2023-11-30 22:04:35 +0700

    update api

--- Modall/User.modal.js
+++ Modall/User.modal.js
@@ -1,6 +1,7 @@
 const db=require("../Connect/Conectdb");
 const bcrypt = require('bcrypt');
-
+const otpGenerator = require('otp-generator'); // Cài đặt bằng cách sử dụng: npm install otp-generator
+const twilio = require('twilio');
 
 const Users=function(Users){
     this.IDUser=Users.IDUser;
@@ -8,6 +9,7 @@ const Users=function(Users){
     this.PassWord=Users.PassWord
     this.Email=Users.Email;
     this.Birthday=Users.Birthday;
+    this.Phone=Users.Phone;
 }
 
 Users.get_all=function(result){
@@ -49,25 +51,66 @@ Users.remove = function (id, result) {
         }
     });
 }
+//
+//
+// Users.create=function(data,result){
+//     // Hash mật khẩu trước khi thêm vào cơ sở dữ liệu
+//     bcrypt.hash(data.PassWord, 10, function (err, hashedPassword) {
+//         if (err) {
+//             result(null);
+//         } else {
+//             // Thay thế mật khẩu chưa mã hóa bằng mật khẩu đã được mã hóa
+//             data.PassWord = hashedPassword;
+//     db.query("INSERT INTO Users SET ?", data,function(err,Users){
+//         if(err){
+//             result(null);
+//         }
+//         else{
+//             result({IDUser: Users.insertId, ...data});
+//         }
+//     });
+//         }
+//     });
+// }
 
+Users.create = function (data, result) {
+    const generatedOTP = otpGenerator.generate(6, { digits: true, alphabets: false, upperCase: false, specialChars: false });
 
-Users.create=function(data,result){
-    // Hash mật khẩu trước khi thêm vào cơ sở dữ liệu
     bcrypt.hash(data.PassWord, 10, function (err, hashedPassword) {
         if (err) {
             result(null);
         } else {
-            // Thay thế mật khẩu chưa mã hóa bằng mật khẩu đã được mã hóa
             data.PassWord = hashedPassword;
-    db.query("INSERT INTO Users SET ?", data,function(err,Users){
-        if(err){
-            result(null);
-        }
-        else{
-            result({IDUser: Users.insertId, ...data});
+
+            // Lưu mã OTP trong cơ sở dữ liệu hoặc bộ lưu trữ tạm thời (như Redis) để xác minh sau này.
+
+            db.query("INSERT INTO Users SET ?", data, function (err, Users) {
+                if (err) {
+                    result(null);
+                } else {
+                    result({ IDUser: Users.insertId, ...data });
+
+                    // Gửi mã OTP đến số điện thoại người dùng bằng Twilio
+                    sendOTPToUser(data.Phone, generatedOTP);
+                }
+            });
         }
     });
-        }
+};
+
+
+// Thêm hàm gửi mã OTP đến số điện thoại người dùng bằng Twilio
+function sendOTPToUser(phone, otp) {
+    const twilioClient = new twilio('YOUR_TWILIO_ACCOUNT_SID', 'YOUR_TWILIO_AUTH_TOKEN');
+
+    twilioClient.messages.create({
+        body: `Mã OTP của bạn là: ${otp}`,
+        to: `+${phone}`, // Số điện thoại người dùng, Twilio yêu cầu định dạng số điện thoại "+123456789"
+        from: 'YOUR_TWILIO_PHONE_NUMBER'
+    }).then(message => {
+        console.log(message.sid);
+    }).catch(error => {
+        console.error(error);
     });
 }
 
@@ -75,7 +118,7 @@ Users.update=function(array,result){
     if (db.state === 'disconnected') {
         db.connect();
     }
-    db.query("UPDATE Users SET UserName=?,PassWord=?,Email=?,Birthday=? WHERE IDUser=?", [array.UserName,array.PassWord,array.Email,array.Birthday,array.IDUser],function(err,Admin){
+    db.query("UPDATE Users SET UserName=?,PassWord=?,Email=?,Birthday=?,Phone=? WHERE IDUser=?", [array.UserName,array.PassWord,array.Email,array.Birthday,array.Phone,array.IDUser],function(err,Admin){
         if(err){
             result(null);
         }
